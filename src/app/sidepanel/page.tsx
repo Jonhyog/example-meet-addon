a7570114a231408e54dafa7908a9c2e605bbddf7
João Miguel <jm.og@hotmail.com>
2025-05-27 00:07:30 -0300

    chore: test only with text

--- src/app/sidepanel/page.tsx
+++ src/app/sidepanel/page.tsx
@@ -1,37 +1,38 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import {
-  meet,
-  MeetSidePanelClient,
-} from '@googleworkspace/meet-addons/meet.addons';
-import { CLOUD_PROJECT_NUMBER, MAIN_STAGE_URL } from '../../constants';
+// import { useEffect, useState } from 'react';
+// import {
+//   meet,
+//   MeetSidePanelClient,
+// } from '@googleworkspace/meet-addons/meet.addons';
+// import { CLOUD_PROJECT_NUMBER, MAIN_STAGE_URL } from '../../constants';
 
 /**
  * See: https://developers.google.com/meet/add-ons/guides/overview#side-panel
  */
 export default function Page() {
-  const [sidePanelClient, setSidePanelClient] = useState<MeetSidePanelClient>();
+  // const [sidePanelClient, setSidePanelClient] = useState<MeetSidePanelClient>();
 
   // Launches the main stage when the main button is clicked.
   async function startActivity() {
-    if (!sidePanelClient) {
-      throw new Error('Side Panel is not yet initialized!');
-    }
-    await sidePanelClient.startActivity({ mainStageUrl: MAIN_STAGE_URL });
+    console.log("Hello world from sidepanel");
+    // if (!sidePanelClient) {
+    //   throw new Error('Side Panel is not yet initialized!');
+    // }
+    // await sidePanelClient.startActivity({ mainStageUrl: MAIN_STAGE_URL });
   }
 
   /**
    * Prepares the Add-on Side Panel Client.
    */
-  useEffect(() => {
-    (async () => {
-      const session = await meet.addon.createAddonSession({
-        cloudProjectNumber: CLOUD_PROJECT_NUMBER,
-      });
-      setSidePanelClient(await session.createSidePanelClient());
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const session = await meet.addon.createAddonSession({
+  //       cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+  //     });
+  //     setSidePanelClient(await session.createSidePanelClient());
+  //   })();
+  // }, []);
 
   return (
     <>
